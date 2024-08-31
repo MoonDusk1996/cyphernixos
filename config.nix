8851ece8e07e7f0e248be14aaae43c658b1751db
Dusk <crepusculop2p@proton.me>
2024-08-31 18:20:49 -0400

    [FIX]: remove allowUnfree

--- config.nix
+++ config.nix
@@ -3,9 +3,6 @@
   # Unshow warning
   home.enableNixpkgsReleaseCheck = false;
 
-  # Enable unfree packages
-  allowUnfree = true;
-
   # ColorScheme
   colorScheme = inputs.nix-colors.colorSchemes.gruvbox-material-dark-hard;
 }
