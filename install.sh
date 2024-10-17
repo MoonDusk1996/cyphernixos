23891ed5696c8c1e107fe27dd2387f9318c43c4b
Dusk <crepusculop2p@proton.me>
2024-10-16 21:38:57 -0400

    fix: install.sh

--- install.sh
+++ install.sh
@@ -10,19 +10,19 @@ if [ -z "$NEWHOSTNAME" ]; then
 fi
 
 # Mover o conteúdo de /etc/nixos para um diretório de backup
-sudo mkdir -p /etc/nixos_bkp
-sudo mv /etc/nixos/* /etc/nixos_bkp/
+sudo mv /etc/nixos /etc/nixos_bkp
 
-# Criar o novo diretório /etc/nixos se não existir
+# Criar o novo diretório /etc/nixos 
 sudo mkdir -p /etc/nixos
 
 # Mover o conteúdo do diretório atual para /etc/nixos
 sudo mv ./* /etc/nixos
 
+# Definir dono do diretorio de configuração nix
 sudo chown "$USER":users -R /etc/nixos
+
 # Gerar o hardware-configuration.nix
-sudo rm -rf /etc/nixos/nixos/hardware-configuration.nix
-sudo nixos-generate-config --dir /etc/nixos/nixos
+sudo nixos-generate-config --dir --force /etc/nixos/nixos
 
 # Executar home-manager e nixos-rebuild com o nome do host fornecido
 home-manager --flake /etc/nixos switch --experimental-features 'nix-command flakes' &&
