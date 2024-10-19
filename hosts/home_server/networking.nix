071494f71dcf3b31102b9819abcae32751501345
Dusk <crepusculop2p@proton.me>
2024-10-19 10:50:05 -0400

    fix: networking

--- hosts/home_server/networking.nix
+++ hosts/home_server/networking.nix
@@ -1,34 +1,6 @@
 {
   networking = {
     hostName = "wired"; # Define your hostname.
-    networkmanager.enable = true;
-    useDHCP = false;
-
-    interfaces.enp3s0 = {
-      useDHCP = false;
-      ipv4.addresses = [
-        {
-          address = "192.168.1.10";
-          prefixLength = 24;
-        }
-      ];
-      # ipv6.addresses = [
-      #   {
-      #     address = "fe80::10";
-      #     prefixLength = 64;
-      #   }
-      # ];
-    };
-    defaultGateway = {
-      address = "192.168.1.1";
-      interface = "enp3s0";
-    };
-    # defaultGateway6 = {
-    #   address = "fe80::1";
-    #   interface = "enp3s0"; # Substitua por sua interface real
-    # };
-    nameservers = [ "127.0.0.1" "8.8.8.8" ];
-
     firewall = rec {
       enable = false;
       allowedTCPPortRanges = [
