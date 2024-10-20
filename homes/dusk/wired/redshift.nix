807f9ddf5216a5530b3fafa7b224282c1a1c20b3
Dusk <crepusculop2p@proton.me>
2024-10-20 16:19:40 -0400

    test

--- homes/dusk/wired/redshift.nix
+++ homes/dusk/wired/redshift.nix
@@ -7,13 +7,12 @@
       night = 3500;
     };
     # General settings
-    brightness = {
-      day = "1";
-      night = "0.9";
+    settings = {
+      redshift.brightness-day = "1";
+      redshift.brightness-night = "0.9";
     };
-
     # Schedule settings
     dawnTime = "6:00-7:45";
-    duskTime = "18:35-20:15";
+    duskTime = "16:35-19:15";
   };
 }
